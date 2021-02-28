a33a76f639875d6c7cb3b7486ee6b657d00e1609
JonathanIlori <63069968+JonathanIlori@users.noreply.github.com>
2021-02-28 15:35:35 -0600

    Update log_parsing.py

--- log_parsing.py
+++ log_parsing.py
@@ -1,1 +1,12 @@
+#Jonathan Part 5 and 6
+from collections import Counter
 
+# Read file directly into a Counter
+with open('file') as f:
+    cnts = Counter(l.strip() for l in f)
+
+# Display 3 most common lines
+cnts.most_common(3)
+
+# Display 3 least common lines
+cnts.most_common()[-3:]
