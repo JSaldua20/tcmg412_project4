0942b5acc6b9dd376b1b778d15110e744f52e452
tazmaniandevil64 <44484776+tazmaniandevil64@users.noreply.github.com>
2021-03-03 20:52:54 -0600

    Add beginning of file seperation
    I got my portion to work on my environment, right now i'm just adding it to the final product to make sure it works before I finish it

--- log_parsing.py
+++ log_parsing.py
@@ -16,7 +16,7 @@ else:
 
 #Jonathan Part 5 and 6
 # Read file directly into a Counter
-with open('file') as f:
+with open(FilePath) as f:
     cnts = Counter(l.strip() for l in f)
 
 # Display 3 most common lines
@@ -24,3 +24,18 @@ cnts.most_common(3)
 
 # Display 3 least common lines
 cnts.most_common()[-3:]
+
+#Tanner splitting file into more files
+input = open(FilePath, "r")
+outputJ = open("JanuaryLog.txt", "w")
+outputF = open("FebuaryLog.txt", "w")
+
+for line in input:
+	if "/Jan/" in line:
+		outputJ.write(line)
+	elif "/Feb/" in line:
+		outputF.write(line)
+
+input.close()
+outputJ.close()
+outputF.close()
