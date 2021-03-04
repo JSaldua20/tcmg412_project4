511b72f07451be7b0b4beddf5f551985fb3869b8
tazmaniandevil64 <44484776+tazmaniandevil64@users.noreply.github.com>
2021-03-03 20:47:22 -0600

    Add file check
    Make sure we have the file on hand to work with before starting anything

--- log_parsing.py
+++ log_parsing.py
@@ -1,6 +1,20 @@
-#Jonathan Part 5 and 6
+import os
+import urllib.request
 from collections import Counter
 
+##Start the file by making sure the file is available
+FilePath = './http_access_log.txt'
+FileExists = os.path.exists(FilePath)
+
+if FileExists == False:
+    print("It doesn't appear you have the data, downloading it now")
+    url = "https://s3.amazonaws.com/tcmg476/http_access_log"
+    urllib.request.urlretrieve(url,"./http_access_log.txt")
+
+else:
+    print ("It looks like you already have the file")
+
+#Jonathan Part 5 and 6
 # Read file directly into a Counter
 with open('file') as f:
     cnts = Counter(l.strip() for l in f)
